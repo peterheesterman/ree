3d8e686d9ca63720468789de890ae323dbd32a76
Peter Heesterman <heesterman.peter@gmail.com>
2019-03-13 21:19:16 +1300

    feat(Setup): list contents of a hard coded path dir
    * print in blue (directory) and green (file)
    * recursive list all contents of folder

---
+++ src/main.rs
@@ -0,0 +1,37 @@
+use std::fs;
+use colored::*;
+use std::path::Path;
+
+fn main() {
+    let path = String::from("./test-folder");
+    list_paths(path, 0);
+}
+
+fn padding_for_depth(depth: i32) -> String {
+    format!("{}", (0..2*depth).map(|_| " ").collect::<String>())
+}
+
+fn list_paths(path: String, depth: i32) {
+    let folder_path = Path::new(&path);
+    let folder_parent_path = folder_path.parent().unwrap();
+    println!(
+        "{}/{}",
+        padding_for_depth(depth),
+        folder_path.strip_prefix(folder_parent_path.to_str().unwrap()).unwrap().to_str().unwrap()
+            .blue()
+            .bold()
+    );
+    let depth = depth + 1;
+    
+    let sub_paths = fs::read_dir(&path).unwrap();
+    for path in sub_paths {
+        let path = format!("{}", path.unwrap().path().display());
+        let metadata = fs::metadata(path.clone()).unwrap();
+        if metadata.is_dir() {
+            list_paths(String::from(path), depth)
+        } else {
+            let file_name = Path::new(&path).file_name().unwrap();
+            println!("{}{}",  padding_for_depth(depth), file_name.to_str().unwrap().green());
+        };
+    }
+}
